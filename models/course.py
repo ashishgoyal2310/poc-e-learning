125e5d0b3a8a90003ca0fe28224d19fbc1447548
Ashish Goyal <ashish2.goyal@orange.com>
2021-03-22 14:29:38 +0530

    Update for courses list api

--- models/course.py
+++ models/course.py
@@ -1,4 +1,5 @@
 from models.database import db
+from datetime import datetime as dt
 
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -37,3 +38,19 @@ class Course(db.Model):
 
     def __repr__(self):
         return '<Course %r>' % self.title
+
+    def as_json(self):
+        dct = {}
+        for field in {'id','description','image_path','on_discount','title'}:
+            dct[field] = getattr(self, field, '')
+
+        for field in {'date_created','date_updated'}:
+            value = getattr(self, field, '')
+            if value:
+                value = dt.strftime(value, "%Y-%m-%d %H:%M:%S")
+            dct[field] = value
+
+        for field in {'discount_price','price'}:
+            dct[field] = str(getattr(self, field, ''))
+
+        return dct
--- routes/course.py
+++ routes/course.py
@@ -5,9 +5,11 @@ from flask import current_app as app
 from flask import request, redirect, render_template, make_response, jsonify
 from http import HTTPStatus
 from datetime import datetime as dt
+from sqlalchemy import or_, and_, not_
 import data
 
 from models.database import db
+from models.course import Course
 
 
 @app.route('/', methods=['GET'])
@@ -32,10 +34,20 @@ def get_course(id):
     """
     -------------------------------------------------------------------------
     Challenge notes:
-    -------------------------------------------------------------------------   
+    -------------------------------------------------------------------------
     1. Bonus points for not using a linear scan on your data structure.
     """
     # YOUR CODE HERE
+    course_obj = db.session.query(Course).filter_by(id=id).first()
+
+    if not course_obj:
+        data = {"messge": "Course {} does not exist".format(id)}
+        return make_response(jsonify(data), 404)
+
+    data = {
+        "data": course_obj.as_json()
+    }
+    return make_response(jsonify(data), 200)
 
 
 @app.route("/course", methods=['GET'])
@@ -62,6 +74,36 @@ def get_courses():
        requests/second.
     """
     # YOUR CODE HERE
+    page_number = request.args.get('page-number', '1')
+    page_number = int(page_number) if page_number.isdigit() else 1
+    page_size = request.args.get('page-size', '10')
+    page_size = int(page_size) if page_size.isdigit() else 10
+    title_words = request.args.get('title-words', '')
+    title_words = [word.strip() for word in title_words.split(',')] if title_words else []
+
+    course_qs = db.session.query(Course)
+
+    filter_group = []
+    for search_word in title_words:
+        filter_group.append(Course.title.like('%{}%'.format(search_word)))
+        # filter_group.append(Course.description.like('%{}%'.format(search_word)))
+
+    if filter_group:
+        course_qs = course_qs.filter(or_(*filter_group))
+
+    page_data = course_qs.paginate(page_number, page_size, error_out=False)
+
+    data = {
+        "data": [course_obj.as_json() for course_obj in page_data.items],
+        "metadata": {
+            "count": len(page_data.items),
+            "page_count": page_data.pages,
+            "page_number": page_number,
+            "page_size": page_data.per_page,
+            "record_count": course_qs.count()
+        },
+    }
+    return make_response(jsonify(data), 200)
 
 
 @app.route("/course", methods=['POST'])
